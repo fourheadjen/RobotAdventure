cde3fe5f626bd4a551b71d9d5bc36abe0cad3c7c
skylardonlevy <skylar.donlevy77@gmail.com>
2015-04-29 18:56:45 -0500

    first commit

---
+++ RobotAdventure/src/game/RobotFrame.java
@@ -0,0 +1,39 @@
+package game;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class RobotFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9039879089105886906L;
+
+	public RobotFrame() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RobotFrame(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RobotFrame(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RobotFrame(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("lol");
+	}
+
+}
