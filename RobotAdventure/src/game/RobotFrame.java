dfd074762ce615ae7a9aa17966137ed13fac2e59
skylardonlevy <skylar.donlevy77@gmail.com>
2015-04-29 19:18:54 -0500

    uhghughguh

--- RobotAdventure/src/game/RobotFrame.java
+++ RobotAdventure/src/game/RobotFrame.java
@@ -34,6 +34,7 @@ public class RobotFrame extends JFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("test1");
+		System.out.println("ugh");
 	}
 
 }
