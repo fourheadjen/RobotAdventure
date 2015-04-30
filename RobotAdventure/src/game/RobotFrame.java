7d5fcd7838bd13f184f48c97b41cda721607490b
skylardonlevy <skylar.donlevy77@gmail.com>
2015-04-29 19:05:09 -0500

    first change to output

--- RobotAdventure/src/game/RobotFrame.java
+++ RobotAdventure/src/game/RobotFrame.java
@@ -33,7 +33,7 @@ public class RobotFrame extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("lol");
+		System.out.print("test1");
 	}
 
 }
